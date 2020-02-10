fa12b42e345532f79d0be83273fee0bebadefab8
Nihir10dec <nihir10dec@gmail.com>
2020-02-10 17:30:29 +0530

    Add files via upload

---
+++ source.c
@@ -0,0 +1,56 @@
+//#include<stdio.h>
+//
+////void bubble_sort(int a[] , int n)
+////{
+////	int temp=0;
+////	for(int i=0; i<n-1 ; i++)
+////	{
+////		for(int j=0;j<n-1-i;j++)
+////		{
+////			if (a[j]>a[j+1])
+////			{
+////				temp = a[j];
+////				a[j] = a[j+1];
+////				a[j+1] = temp;
+////			}
+////	}
+////}
+////}
+//
+//int main()
+//{
+//	int i,j, a[10], temp, n=10;
+//	int a1[5]={1,21,10,45,7};
+//	int a2[5]={2,89,32,65,4};
+//
+//	for(i=0,j=5; i<5; i++,j++)
+//	{
+//		a[i]=a1[i];
+//		a[j]=a2[i];
+//	}
+//	//bubble_sort(a3 , 10);
+//
+//	for(i=0; i<n-1 ; i++)
+//	{
+//		for(j=0;j<n-1-i;j++)
+//		{
+//			if (a[j]>a[j+1])
+//			{
+//				temp = a[j];
+//				a[j] = a[j+1];
+//				a[j+1] = temp;
+//			}
+//	}
+//}
+//
+//
+//	for(i=0; i<10; i++)
+//	{
+//		printf("%d \n", a[i]);
+//	}
+//
+//
+//
+//getch();
+//return 0;
+//}
