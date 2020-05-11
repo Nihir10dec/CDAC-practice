a7712c471056a7e4dd8cfdd5eeee9b167fff8d25
Nihir10dec <nihir10dec@gmail.com>
2020-05-11 18:25:57 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+function highest_number(a , b , c){
+    return Math.max(a,b,c);
+}
+
+function show_name() {
+    document.getElementById("name").innerHTML = "Nihir Shah";
+    console.log("Nihir Shah");
+    alert("Nihir Shah");
+}
+
+function diff_headers(){
+    document.getElementById("diff_header").innerHTML ="<h1>h1 Nihir</h1> <h2>h2 Nihir</h2> <h3>h3 Nihir</h3> <h4>h4 Nihir</h4> <h5>h5 Nihir</h5>" ;
+}
